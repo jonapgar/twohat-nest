ed8fd72b5457b02fabeacb3acf25805c9cde51d6
jonapgar <5638966+jonapgar@users.noreply.github.com>
2020-09-15 15:41:01 -0400

    done

--- src/resolvers/message.resolver.ts
+++ src/resolvers/message.resolver.ts
@@ -1,18 +1,25 @@
 import { Parent, Query, ResolveField, Resolver, ArgsType, Field, Int, Args } from '@nestjs/graphql'
 import { Message } from '../models/message.model'
 import { PrismaService } from '../prisma/prisma.service'
+import { Topic } from 'src/models/topic.model';
+import { MessageWhereInput } from '@prisma/client';
 
+const DEFAULT_MIN_CONFIDENCE=224
+const DEFAULT_MIN_RELEVANCE=32
 
 @ArgsType()
 class MessageArgs  {
   
 
 
-  @Field((type) => Int)
-  topic?:number
+  @Field((type) => [Int])
+  topics:number[] = []
+  @Field((type) => [Int])
+  confidence:number = DEFAULT_MIN_CONFIDENCE
+  @Field((type) => [Int])
+  relevance:number = DEFAULT_MIN_RELEVANCE
   @Field((type) => Int)
   offset = 0;
-
   @Field((type) => Int)
   limit = 10;
 }
@@ -20,11 +27,19 @@ class MessageArgs  {
 export class MessageResolver {
   constructor(private prisma: PrismaService) {}
   @Query(() => [Message])
-  async messages(@Args() {offset,limit,topic}:MessageArgs) {
-    const options = topic ? {where:{
-      Topic:{some:{topic}}
-     }}:undefined
-    return this.prisma.message.findMany({skip:offset,take:limit,...options})
+  async messages(@Args() {offset,limit,topics,confidence,relevance}:MessageArgs) {
+    const where:MessageWhereInput | undefined = topics.length ? {
+      AND:topics.map(topic=>{
+        return {
+          Topic:{
+            some:{
+            topic,
+            confidence:{gte:confidence},
+            relevance:{gte:relevance}
+        }}}
+      })
+     }:undefined
+    return this.prisma.message.findMany({skip:offset,take:limit,where})
   }
 
   @ResolveField()
--- src/resolvers/topic.resolver.ts
+++ src/resolvers/topic.resolver.ts
@@ -1,14 +1,29 @@
-import { Args, Int, ResolveField, Parent, Resolver, Query } from '@nestjs/graphql'
-
+import { Query, Resolver, ArgsType, Args, Int, Field } from '@nestjs/graphql'
 import { Topic } from '../models/topic.model'
-
 import { PrismaService } from '../prisma/prisma.service'
+import { TopicWhereInput } from '@prisma/client'
 
+
+
+@ArgsType()
+class TopicArgs  {
+  @Field((type) => [Int])
+  topics:number[] = []
+  @Field((type) => [Int])
+  confidence = 0
+  @Field((type) => [Int])
+  relevance = 0
+}
 @Resolver(() => Topic)
 export class TopicResolver {
   constructor(private prisma: PrismaService) {}
   @Query(() => [Topic])
-  async topics() {
-    return this.prisma.topic.findMany()
+  async topics(@Args() {topics,confidence,relevance}:TopicArgs) {
+    const where:TopicWhereInput ={
+      topic:topics.length ? {in:topics}:undefined,
+      relevance:{gte:relevance},
+      confidence:{gte:confidence}
+    }
+    return this.prisma.topic.findMany({where})
   }
 }
