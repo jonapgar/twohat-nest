b551c18db11cb72c33a129d294a1f7c678f286e5
jonapgar <5638966+jonapgar@users.noreply.github.com>
2020-09-15 13:43:35 -0400

    paginate and query messages

--- src/resolvers/message.resolver.ts
+++ src/resolvers/message.resolver.ts
@@ -1,14 +1,30 @@
-import { Args, Int, ResolveField, Parent, Resolver, Query } from '@nestjs/graphql'
-
+import { Parent, Query, ResolveField, Resolver, ArgsType, Field, Int, Args } from '@nestjs/graphql'
 import { Message } from '../models/message.model'
 import { PrismaService } from '../prisma/prisma.service'
 
+
+@ArgsType()
+class MessageArgs  {
+  
+
+
+  @Field((type) => Int)
+  topic?:number
+  @Field((type) => Int)
+  offset = 0;
+
+  @Field((type) => Int)
+  limit = 10;
+}
 @Resolver(() => Message)
 export class MessageResolver {
   constructor(private prisma: PrismaService) {}
   @Query(() => [Message])
-  async messages() {
-    return this.prisma.message.findMany()
+  async messages(@Args() {offset,limit,topic}:MessageArgs) {
+    const options = topic ? {where:{
+      Topic:{some:{topic}}
+     }}:undefined
+    return this.prisma.message.findMany({skip:offset,take:limit,...options})
   }
 
   @ResolveField()
